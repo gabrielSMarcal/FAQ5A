1ca179ec6aa6d05a0f57cb7bdd9cbc10ff7ca984
GabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2026-01-03 11:37:02 -0300

    Criação da pasta scripts para o modo escuro e adição da página adicionar

--- faq/views.py
+++ faq/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'faq/index.html')
+
+def adicionar(request):
+    if request.method == 'POST':
+        # Lógica para processar o formulário de adição de FAQ
+        pass
+    return render(request, 'faq/adicionar.html')
